0bb385658051239c7928182e2c86010d1cef09c4
David Trussler <mail@davidtrussler.net>
2021-01-31 23:42:51 +0000

    Adds database file

---
+++ src/db/index.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv'); 
+const mysql = require('mysql'); 
+
+dotenv.config(); 
+
+let connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+
+		return;
+	}
+
+	console.log('Database connected as id ' + connection.threadId);
+});
+
+function fetchContent(sublinkId, callback) {
+	let query = 'SELECT content FROM sublinks WHERE sublinkid=' + sublinkId;
+
+	return new Promise((resolve, reject) => {
+		connection.query(query, (err, res) => {
+			if (err) {
+				reject(err); 
+			} else {
+				resolve(res); 
+			}
+		})
+	}).then((response) => {
+		callback(
+			response[0]
+		)
+	}).catch(reject => {
+		return reject;
+	})
+}; 
+
+module.exports = {
+	fetchContent
+}; 
